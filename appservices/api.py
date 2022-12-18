1508e159026adecf65a0755bf5d903123795f720
Gefferson Casasola <gcasasolah@uni.pe>
2022-12-18 12:51:23 -0500

    creacion de vistas y crud de services

--- appservices/api.py
+++ appservices/api.py
@@ -2,15 +2,29 @@ from .models import Service
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import ServiceSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework import status
 from rest_framework.authentication import BasicAuthentication
+from rest_framework import viewsets
+from .pagination import StandardResultsSetPagination
 
+#LISTA DE SERVICIOS PARA EL USUARIO
 class GetAllService(APIView):
-    authentication_classes=[BasicAuthentication]
     permission_classes = [IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
     throttle_scope = 'services'
 
     def get(self, request):
         services = Service.objects.all()
         serializer = ServiceSerializer(services, many = True)
         return Response(serializer.data)
+  
+#CRUD ADMIN
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    pagination_class = StandardResultsSetPagination
+    permission_classes=[IsAdminUser]
+    
+    
+    
--- appservices/urls.py
+++ appservices/urls.py
@@ -1,12 +1,13 @@
-from .api import GetAllService
+from .api import GetAllService, ServiceViewSet
 from django.urls import path
 from rest_framework import routers
 
-#router = routers.DefaultRouter()
-
+router = routers.DefaultRouter()
+router.register('api/v2/service-payments/crud', ServiceViewSet, 'crud-services')
 urlpatterns = [
     
     path('api/v2/service-payments/services/', GetAllService.as_view(),name='getAllService'),
+    
 ]
 
-#urlpatterns = router.urls
+urlpatterns += router.urls
