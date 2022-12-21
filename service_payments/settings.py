a128cf25d31d68180974c5b87bdf0b3fd43269ad
Gefferson Casasola <gcasasolah@uni.pe>
2022-12-20 21:50:07 -0500

    documentacion del proyecto

--- service_payments/settings.py
+++ service_payments/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     "rest_framework",
     "rest_framework_simplejwt",  # users
     "rest_framework.authtoken",  # users
--- service_payments/urls.py
+++ service_payments/urls.py
@@ -17,6 +17,24 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from versionedServicePayments.v1.router import api_urlpatterns as api_v1
 from versionedServicePayments.v2.router import api_urlpatterns as api_v2
+from django.conf.urls.static import static
+from django.conf import settings
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ServicePayments API",
+      default_version='v1',
+      description="Proyecto SERVICE PAYMENTS de Gefferson Casasola",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +43,7 @@ urlpatterns = [
     path('', include('appservices.urls')),
     re_path(r'^api/v1/', include(api_v1)),
     re_path(r'^api/v2/', include(api_v2)),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
